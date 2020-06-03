fdceb7999119a2bd4670637c23cd152a98e7a3ae
koheisuzuki <k.s-coward-0629@ezweb.ne.jp>
2020-06-03 21:55:21 +0900

    admin user view追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,13 @@
 class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all.order(id: "ASC")
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -17,20 +26,22 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update
-  end
-
-  def show
     @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
-  def index
-    @users = User.all.order(id: "DESC")
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_cofirmation)
+      params.require(:user).permit(:name, :email, :admin, :password, :password_cofirmation)
     end
 end
