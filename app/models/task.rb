4e026dae99bfdcd57cc5500899a1df5af71120b5
koheisuzuki <k.s-coward-0629@ezweb.ne.jp>
2020-06-05 07:21:07 +0900

    関連付け user tasks 追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
 
+  belongs_to :user
+
   private
     def set_nameless_name
       self.name = "名前なし" if name.blank?
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
+  
   has_secure_password
+  has_many :tasks
 end
