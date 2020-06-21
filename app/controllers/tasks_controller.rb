49cee3d40c58ef7c275284f3d916062820ad2da9
koheisuzuki <k.s-coward-0629@ezweb.ne.jp>
2020-06-22 08:01:18 +0900

    とりあえずほぼ全部

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,18 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = current_user.tasks.order(created_at: :desc)
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true).page(params[:page])
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv" }
+    end
+  end
+
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスク追加しました"
   end
 
   def show    
@@ -14,11 +25,24 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
+      TaskMailer.creation_email(@task).deliver_now
+      SampleJob.perform_later
       redirect_to tasks_path, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
   def edit
@@ -31,7 +55,6 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
@@ -40,6 +63,6 @@ class TasksController < ApplicationController
     end
 
     def task_params
-      params.require(:task).permit(:name, :description)
+      params.require(:task).permit(:name, :description, :image)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
-  before_validation :set_nameless_name
+  # before_validation :set_nameless_name
+  # paginates_per 50
+  has_one_attached :image
 
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
@@ -8,8 +10,37 @@ class Task < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc) }
 
-  private
-    def set_nameless_name
-      self.name = "名前なし" if name.blank?
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr)}
+      end
     end
+  end
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      task = new
+      task.attributes = row.to_hash.slice(*csv_attributes)
+      task.save!
+    end
+  end
+
+  # private
+  #   def set_nameless_name
+  #     self.name = "名前なし" if name.blank?
+  #   end
 end
